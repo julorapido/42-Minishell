f7c1229e5d2844e91e763bbe0727b32801b92516
Jules Sainthorant <jsaintho@made-f0Ar7s10.clusters.42paris.fr>
2024-09-26 18:13:49 +0200

    p

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: jsaintho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 17:46:03 by jsaintho          #+#    #+#             */
-/*   Updated: 2024/09/26 18:05:51 by jsaintho         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:13:35 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,49 @@ static enum TOKEN_TYPE	switcher(char *token)
 }
 
 
+static char *space_line(char *cmd_line)
+{
+	char *new;
+	int  i;
+	int  j;
+
+	i = 0;
+	j = 0;
+	new = space_alloc(line);
+	while (new && line[i])
+	{
+		if (quotes(line, i) != 2 && line[i] == '$' && i && line[i - 1] != '\\')
+		new[j++] = (char)(-line[i++]);
+		else if (quotes(line, i) == 0 && is_sep(line, i))
+		{
+	 		new[j++] = ' ';
+			new[j++] = line[i++];
+			if (quotes(line, i) == 0 && line[i] == '>')
+				new[j++] = line[i++];
+				new[j++] = ' ';
+		}
+		else
+			new[j++] = line[i++];
+	 }
+	 new[j] = '\0';
+ 	_memdel(line);
+ 	return (new);
+}
+
 void	parse_command(char *cmd, token **cmd_tokens)
 {
-	char	**parsed_cmd;
+	char	*spaced_cmd;
+	char	**split_cmd;
 	token	*t;
 	int		i;
 
-	parsed_cmd = ft_split(cmd, ' ');
+	spaced_cmd = space_line(cmd);
+	split_cmd = ft_split(spaced_cmd, ' ');
 	i = 0;
-	while (parsed_cmd[i])
+	while (split_cmd[i])
 	{
 		t = token_new(
-			parsed_cmd[i],
+			split_cmd[i],
 			switcher(cmd[i])
 		);
 		token_push(cmd_tokens, t);
